ef4d961615ef5c14c54bcff51f5561b22467c14a
Goodness E. Eboh <ebohgoodness.t@gmail.com>
2024-08-13 18:48:52 +0100

    first commit

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-bucket"
+    bucket         = "sock-bucket"
     key            = "terraform/key"
     region         = "us-east-1"
     dynamodb_table = "terraform-lock"
